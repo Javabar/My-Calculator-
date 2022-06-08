dfd9232abee8ce473f434e8eb3ae4a7775b8beb4
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-06-08 11:15:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Operators from './Components/Operators';
+import Numbers from './Components/Numbers';
+import { evaluate } from 'mathjs';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [input, setInput]= useState("");
+
+  // Function to calculate a result on a specific button click and update the state
+
+  const calculate = (button) => {
+    if(button === '='){
+      const result = evaluate(input);
+      setInput(result);
+    } else if (button === 'c'){
+      setInput("")
+    } else 
+    setInput(input + button);
+  };
+
+  // function for when user clicks on buttons
+const handleClick = (e) => {
+  const display = e.target.textContent;
+  calculate(display);
 }
 
+return (
+<div className="App">
+<div>
+<h1>My Calculator </h1>
+<h2>{input}</h2>
+{/* need to pass handle click as a prop down to operator and numbers */}
+<Operators handleClick={handleClick}/>
+<Numbers handleClick={handleClick}/>
+</div>
+</div>
+);
+};
 export default App;
